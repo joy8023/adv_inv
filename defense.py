093ba00a91d6432d33026f3d60f3f035dae89d68
joy8023 <joyjoy8023@gmail.com>
2021-01-28 15:48:35 +0800

    run for whole set

--- defense.py
+++ defense.py
@@ -23,10 +23,8 @@ parser.add_argument('--c', type=float, default=50.)
 parser.add_argument('--num_workers', type=int, default=1, metavar='')
 parser.add_argument('--no-cuda', action='store_true', default=False)
 parser.add_argument('--seed', type=int, default=1, metavar='')
-parser.add_argument('--epsilon', type = float, default = 1, metavar = '')
+parser.add_argument('--epsilon', type = float, default = 10e-5, metavar = '')
 parser.add_argument('--num_step', type = int, default = 10, metavar = '')
-
-
 
 
 def predict(classifier, device, data_loader):
@@ -40,6 +38,8 @@ def predict(classifier, device, data_loader):
 
 	return output
 
+#the function to keep the out similar to the original one
+#at least the classification is the same
 def restore(new_logit, new_label, original_logit, original_label, amplifier = 1.1):
 
 	if original_logit[original_label] > new_logit[new_label]:
@@ -53,31 +53,30 @@ def restore(new_logit, new_label, original_logit, original_label, amplifier = 1.
 def perturb(prediction, epsilon, grad, logit_original):
 
 	sign = grad.sign()
-	print(sign)
 	logit_new = prediction + epsilon * sign 
 	
 	logit_diff = F.mse_loss(logit_new, logit_original)
 	print('*************logit_diff:',logit_diff.item())
-	#print(prediction[0].data)
-	#print(output[0].data)
-
 
 	#calculate accuracy for perturbed images
 	original_label = torch.max(logit_original, 1)[1].cpu().numpy()
 	new_label = torch.max(logit_new, 1)[1].cpu().numpy()
 
 	'''
+	#to use the restore function to replace the max one by one
 	for i in range(64):
 		logit_new[i] = restore(logit_new[i],new_label[i], logit_original[i], original_label[i])
 
 	'''
-	print(logit_original.size())
+
+	#print(logit_original.size())
 	orig_label_onehot = F.one_hot(torch.tensor(original_label), 530)
-	print(orig_label_onehot)
+	#print(orig_label_onehot)
 	orig_label_onehot = torch.tensor(orig_label_onehot, dtype=torch.uint8)
 	#print(orig_label_onehot.size())
 	#new_label_onehot = F.one_hot(torch.tensor(new_label), 530)
 
+	# to keep the max unchanged
 	logit_new[orig_label_onehot] = torch.max(logit_new, 1)[0]*1.1
 
 
@@ -86,39 +85,40 @@ def perturb(prediction, epsilon, grad, logit_original):
 	accu = np.sum(original_label == new_label)/original_label.shape[0]
 
 	print('************accu:',accu)
+
+	'''
 	print(torch.max(logit_original,1)[0].data)
 	print(logit_new[0][original_label[0]].item())
 	print(logit_new[0][new_label[0]].item())
 	print(torch.max(logit_new,1)[0].data)
-
-
+	'''
 
 	output = F.softmax(logit_new, dim=1)
 	output_diff = F.mse_loss(output, F.softmax(prediction, dim=1))
 	print('************output_diff', output_diff.item())
 
-
-
 	return output
 
-
+#main outer function
 def add_noise(classifier, inversion, device, data_loader, epsilon, num_step):
 
 	classifier.eval()
 	inversion.eval()
-	#epsilon = 1
-	#after_noise = []
 
-	for batch_idx, (data, target) in enumerate(data_loader):
+	ori_img = []
+	noise_logit = []
 
-		data, target = data.to(device), target.to(device)
+	for batch_idx, (data_, target_) in enumerate(data_loader):
+
+		data, target = data_.to(device), target_.to(device)
 
 		for i in range(num_step):
 
 			print('========perturbation iteration:',i)
 
 			#prediction = classifier(data, release = True)
-			logit = classifier(data, release = False)
+			with torch.no_grad:
+				logit = classifier(data, release = False)
 		
 			#create new tensor for further perturbation
 			logit = torch.tensor(logit).to(device)
@@ -128,53 +128,44 @@ def add_noise(classifier, inversion, device, data_loader, epsilon, num_step):
 			loss =F.mse_loss(reconstruction, data)
 			loss.backward()
 
-			#print('grad:',logit.grad.data)
 			logit_grad = logit.grad.data
 
 			if i == 0:
 				original_logit = logit
 
-			#print('grad size:', logit_grad.size())
 			perturbation = perturb(logit, epsilon, logit_grad, original_logit)
 			perturbation= perturbation.to(device)
 			pert_recon = inversion(perturbation)
 
+			#for plot use
+			'''
 			if i == 0:
 				truth = data[0:8]
 				inverse = reconstruction[0:8]
 				out = torch.cat((truth, inverse))
-				out = torch.cat((out, pert_recon[0:8]))
+				#out = torch.cat((out, pert_recon[0:8]))
 
-
+			
 			out = out = torch.cat((out, pert_recon[0:8]))
+			'''
 			data = torch.tensor(pert_recon).to(device)
-		break
+
+		ori_img.append(data_)
+		noise_logit.append(perturbation.cpu())
+
+		# only do the first batch
+		#break
+	ori_img = np.array(ori_img)
+	noise_logit = np.array(noise_logit)
+	print(ori_img.shape)
+	print(noise_logit.shape)
 
 	#out = torch.cat((out, torch.tensor(after_noise)))
-	vutils.save_image(out, 'out1/test_epsilon_{}.png'.format(epsilon), normalize=False)
-
-
-		
-			#print(reconstruction[0].data)
-			#print(pert_recon[0].data)
-	'''
-		plot = False
-		if plot:
-			truth = data[0:32]
-			inverse = reconstruction[0:32]
-			defense = pert_recon[0:32]
-			out = torch.cat((truth, inverse))
-			out = torch.cat((out, defense))
-			for i in range(4):
-					out[i * 24 :i * 24 + 8] = truth[i * 8:i * 8 + 8]
-					out[i * 24 + 8:i * 24 + 16] = inverse[i * 8:i * 8 + 8]
-					out[i * 24 + 16:i * 24 + 24] = defense[i * 8:i * 8 + 8]
-			vutils.save_image(out, 'out1/test_epsilon_{}.png'.format(epsilon), normalize=False)
-			plot = False
-	'''
+	#vutils.save_image(out, 'out1/test_epsilon_{}.png'.format(epsilon), normalize=False)
 
 	return
 
+#old version
 def defense(classifier, inversion, device, data_loader, epsilon):
 
 	classifier.eval()
@@ -255,6 +246,37 @@ def inv_test(classifier, inversion, device, data_loader, epoch, msg = 'test'):
 	return mse_loss
 
 
+
+#generate CelebA dataset with defensive logit
+def celeb_logit(path):
+
+	if not os.path.exists(path):
+		print("no such directory")
+		return
+
+	images = []
+	C = 0
+
+	with os.scandir(path) as files:
+		
+		for file in files:
+			if os.path.splitext(file)[1] == '.jpg' and C < 50000:
+				img = cv2.imread(os.path.join(file), cv2.IMREAD_GRAYSCALE)
+				#print(img.shape)
+				img = cv2.resize(img[70:178,35:143],(64,64))
+				#cv2.imshow("img",img)
+				#cv2.waitKey(0)
+				images.append(img)
+				C += 1
+	
+	images = np.array(images)
+	#images = images / 255.0
+
+	print(images.shape)
+	#print(images.max())
+	np.save("celeba_5w_255.npy", images)
+
+
 def main():
 
 	args = parser.parse_args()
@@ -303,13 +325,14 @@ def main():
 	
 	epsilon = args.epsilon
 	num_step = args.num_step
+
 	'''
 	for i in range(10):
 		defense(classifier, inversion, device, celeb_loader,epsilon)
 		epsilon *= 10
 	'''
 	#defense(classifier, inversion, device, celeb_loader,epsilon)
-	add_noise(classifier, inversion, device, celeb_loader,epsilon, num_step)
+	add_noise(classifier, inversion, device, celeb_loader, epsilon, num_step)
 
 
 if __name__ == '__main__':
